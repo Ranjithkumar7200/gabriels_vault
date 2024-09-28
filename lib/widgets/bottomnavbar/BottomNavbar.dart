46d2cbca8cd39fda4e02c1152823fd5f960e49c3
Ranjithkumar7200 <ranjithkumarb8072@gmail.com>
2024-09-28 17:10:56 +0530

    completed project

--- lib/widgets/bottomnavbar/BottomNavbar.dart
+++ lib/widgets/bottomnavbar/BottomNavbar.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, sized_box_for_whitespace, prefer_const_constructors_in_immutables
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sized_box_for_whitespace, prefer_const_constructors_in_immutables
 
 import 'package:flutter/material.dart';
 import 'package:gabriels_vault/screens/constant/ColorsPalette.dart';
@@ -12,6 +12,84 @@ class BottomNavbar extends StatelessWidget {
     required this.selectedIndex,
     required this.onItemTapped,
   });
+
+  void _openBottomSheet(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      backgroundColor: ColorsPalette.bg,
+      builder: (BuildContext context) {
+        return Container(
+          padding: const EdgeInsets.all(16),
+          height: MediaQuery.of(context).size.height * 0.4,
+          child: Column(
+            children: [
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: 100,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: ColorsPalette.bg,
+                    border: Border.all(
+                        color: const Color.fromARGB(255, 233, 229, 229))),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.add_circle_outline,
+                      color: ColorsPalette.primary,
+                    ),
+                    Text(
+                      'Create New Message from Scratch',
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: 100,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: ColorsPalette.bg,
+                    border: Border.all(
+                        color: const Color.fromARGB(255, 233, 229, 229))),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.filter_frames_outlined,
+                      color: ColorsPalette.primary,
+                    ),
+                    Text(
+                      'Choose Templates',
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -41,15 +119,20 @@ class BottomNavbar extends StatelessWidget {
           top: -40,
           child: ClipPath(
             clipper: HalfCircleClipper(),
-            child: Container(
+            child: GestureDetector(
+              onTap: () => _openBottomSheet(context),
+              child: Container(
                 height: 50,
                 width: 100,
                 decoration: BoxDecoration(
-                    color: ColorsPalette.bg,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(200),
-                      topRight: Radius.circular(200),
-                    ))),
+                  color: ColorsPalette.bg,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(200),
+                    topRight: Radius.circular(200),
+                  ),
+                ),
+              ),
+            ),
           ),
         ),
         Positioned(
@@ -59,7 +142,7 @@ class BottomNavbar extends StatelessWidget {
             shape: CircleBorder(),
             backgroundColor: ColorsPalette.primary,
             child: Icon(Icons.add, size: 30, color: Colors.white),
-            onPressed: () {},
+            onPressed: () => _openBottomSheet(context),
           ),
         ),
       ],
@@ -83,9 +166,10 @@ class BottomNavbar extends StatelessWidget {
             Text(
               label,
               style: TextStyle(
-                  color: isSelected ? ColorsPalette.primary : Colors.black,
-                  fontSize: 10,
-                  fontWeight: FontWeight.bold),
+                color: isSelected ? ColorsPalette.primary : Colors.black,
+                fontSize: 10,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ],
         ),
